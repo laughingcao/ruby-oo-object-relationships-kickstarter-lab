63fabfa3b147da2192f92f85f09c4fafd5cf0da8
William Cao <williamacao@gmail.com>
2021-02-06 14:36:44 -0800

    commit tree 286e94c8e546a2d9fcbe52cd320910b595d47aca

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+    attr_accessor :backers, :title
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+        @backers << backer
+    end
+
+
+end
